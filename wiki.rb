1b476b52a0f27208cd7a5763c3607915791ddb41
petitlapin86 <skinnerpa86@yahoo.com>
2018-01-23 14:22:07 -0500

    first attempt sinatra

---
+++ wiki.rb
@@ -0,0 +1,56 @@
+require 'sinatra'
+require 'uri'
+
+
+def page_content(title)
+  File.read("pages/#{title}.txt")
+rescue Errno::ENOENT
+  return nil
+end
+
+def save_content(title)
+  File.read("pages/#{title}.txt", "w") do |file|
+    file.print(content)
+  end
+end
+
+def delete_content(title, content)
+  File.delete("pages/#{title}.txt")
+end
+
+get("/") do
+  erb :welcome
+end
+
+get "/new" do
+  erb :new #renders a new erb template and looks for new file
+end
+
+get "/:title" do #setting up get request
+  @title = params[:title] #instance variables
+  @content = page_content(params[@title])
+  erb :show #render erb template
+end
+
+#gets a page title from url parameter load content for that page and then render and html form for that content
+get "/:title/edit" do
+  @title = params[:title]
+  @content = page_content(@title)
+  erb :edit
+end
+
+post "/create" do #setting up post request
+save_content(params["title"], params["content"])
+redirect URI.escape("/#{params["title"]}") #this method tells sinatra to send a redirect response to browser
+end
+
+put "/:title" do
+  save_content(params["title"], params["content"])
+  redirect URI.escape("/#{params["title"]}")
+end
+
+#delete page request
+delete "/:title" do
+  delete_content(params[:title])
+  redirect "/"
+end
